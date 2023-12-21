98c7e29b507bcdcf0b43e4c5e8b59d309ce07903
Khadija Tasnim <khadijatasnim338@gmail.com>
2023-12-21 11:26:22 +0600

    add new code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # i-mybos
-Khadija Tasnim
+Khadija Tasnim Khushi
---
+++ assets/js/main.js
@@ -0,0 +1,139 @@
+(function ($) {
+	"use strict";
+
+
+    var swiper = new Swiper(".testimonial-slider", {
+        slidesPerView: 4,
+        speed: 1500,
+        spaceBetween: 25,
+        autoplay: {
+            delay: 2500, // Autoplay duration in milliseconds
+        },
+        navigation: {
+            nextEl: " .next-01",
+            prevEl: " .prev-01",
+        },
+
+        breakpoints: {
+            280: {
+                slidesPerView: 1,
+            },
+            386: {
+                slidesPerView: 1,
+            },
+            576: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+            992: {
+                slidesPerView: 3,
+            },
+            1200: {
+                slidesPerView: 4,
+            },
+            1400: {
+                slidesPerView: 4,
+            },
+        }
+
+    });
+
+
+}(jQuery));
+
+(function ($) {
+	"use strict";
+
+
+    var swiper = new Swiper(".testimonial-slider2", {
+        slidesPerView: 3,
+        speed: 1500,
+        spaceBetween: 20,
+        
+        
+        autoplay: {
+            delay: 2500, // Autoplay duration in milliseconds
+        },
+        navigation: {
+            nextEl: " .next-01",
+            prevEl: " .prev-01",
+        },
+
+        breakpoints: {
+            280: {
+                slidesPerView: 1,
+            },
+            386: {
+                slidesPerView: 1,
+            },
+            576: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            992: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 3,
+            },
+            1400: {
+                slidesPerView: 3,
+            },
+        }
+
+    });
+
+
+}(jQuery));
+
+(function ($) {
+	"use strict";
+
+
+    var swiper = new Swiper(".testimonial-slider3", {
+        slidesPerView: 3,
+        speed: 1500,
+        spaceBetween: 20,
+        
+        
+        autoplay: {
+            delay: 2500, // Autoplay duration in milliseconds
+        },
+        navigation: {
+            nextEl: " .next-01",
+            prevEl: " .prev-01",
+        },
+
+        breakpoints: {
+            280: {
+                slidesPerView: 1,
+            },
+            386: {
+                slidesPerView: 1,
+            },
+            576: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 1,
+            },
+            992: {
+                slidesPerView: 1,
+            },
+            1200: {
+                slidesPerView: 2,
+            },
+            1400: {
+                slidesPerView: 2,
+            },
+        }
+
+    });
+
+
+}(jQuery));
+
