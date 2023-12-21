043664a06597fbd562a1b0b7f254204d861bf0fc
Khadija Tasnim <khadijatasnim338@gmail.com>
2023-12-21 19:01:45 +0600

    Add new code

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,4 +136,50 @@
 
 
 }(jQuery));
+(function ($) {
+	"use strict";
+
+
+    var swiper = new Swiper(".testimonial-slider-4", {
+        slidesPerView: 2,
+        speed: 1500,
+        spaceBetween: 10,
+        
+        
+        autoplay: {
+            delay: 2500, // Autoplay duration in milliseconds
+        },
+        navigation: {
+            nextEl: " .next-01",
+            prevEl: " .prev-01",
+        },
+
+        breakpoints: {
+            280: {
+                slidesPerView: 3,
+            },
+            386: {
+                slidesPerView: 3,
+            },
+            576: {
+                slidesPerView: 3,
+            },
+            768: {
+                slidesPerView: 4,
+            },
+            992: {
+                slidesPerView: 4,
+            },
+            1200: {
+                slidesPerView: 6,
+            },
+            1400: {
+                slidesPerView: 6,
+            },
+        }
+
+    });
+
+
+}(jQuery));
 
